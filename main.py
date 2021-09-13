4b432ac615757c685d1bba6e3622da52bcca4245
Benedetta-Casavecchia <81049742+Benedetta-Casavecchia@users.noreply.github.com>
2021-09-13 11:16:07 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+#MAIN
+
+simultype = ['', '-Al']
+#simultype = ['_turb', '2AlTurb']
+#simultype = ['_rad', '2-rad']
+#simultype = ['_turb_rad', '2AlTurb-rad']
+
+#DOWN-THE-BARREL
+
+#ray_start = [0, -2, 0]
+#ray_end   = [0, 22, 0]
+#orient    = '-dtb'
+
+#EDGE-ON
+
+ray_start = [0., 10., -6.]
+ray_end   = [0., 10., 6.]
+orient    = '-eo'
+
+for i in range(4):
+    if i == 0:
+        tstep = 0  #initial
+    elif i == 1:
+        tstep = 5  #compression
+    elif i == 2:
+        tstep = 30 #stripping
+    else:
+        tstep = 45 #break-out
+
+    file_name = 'data.' + str(tstep).zfill(4) + '.vtk'
+    input_directory = 'Wind_cloud'+ simultype[0] + '/Wind-Cloud-MHD' + simultype[1] +'/'
+    output_directory = 'Wind_cloud' + simultype[0] + '/Spectral_analysis' + simultype[0] +'/'
+
+    ds = fieldanalysis_yt(file_name, input_directory)
+    spectra(output_directory, ds, tstep, simultype, orient, ray_start, ray_end)
